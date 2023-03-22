3231136cc08318dfabfe89d9c72e77f9434f0673
Soniabensaad <5687@holbertonstudents.com>
2023-03-22 20:42:36 +0100

    airbnb work

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""
-DBstorage using sqlAlchemy
-"""
+"""new storage"""
+from os import getenv
 import models
 from models.amenity import Amenity
 from models.base_model import BaseModel, Base
@@ -10,69 +9,61 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-import sqlalchemy
 from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
-from os import getenv
+from sqlalchemy.orm import sessionmaker, scoped_session
+classes = {"Amenity": Amenity, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
-class DBStorage:
-    """using sqlAlchemy store data"""
+
+class DBStorage():
+    """new storge"""
     __engine = None
     __session = None
-    classes = ["State", "City", "User", "Place", "Review"]
+
     def __init__(self):
-        """create the engine"""
+        """initialization"""
         HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
         HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
         HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
         HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
         HBNB_ENV = getenv('HBNB_ENV')
-        datab_exe = 'mysql+mysqldb://{}:{}@{}/{}'.format(
-                                            HBNB_MYSQL_USER,
-                                            HBNB_MYSQL_PWD,
-                                            HBNB_MYSQL_HOST,
-                                            HBNB_MYSQL_DB
-                                                )
-        self.__engine = create_engine(datab_exe, pool_pre_ping=True)
-        if HBNB_ENV == 'test':
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
+            HBNB_MYSQL_USER, HBNB_MYSQL_PWD, HBNB_MYSQL_HOST, HBNB_MYSQL_DB),
+            pool_pre_ping=True)
+        if HBNB_ENV == "test":
             Base.metadata.drop_all(self.__engine)
 
-
     def all(self, cls=None):
-        """query on the current database session"""
-        dict = {}
-        if cls is None:
-            for q in self.classes:
-                q = eval(q)
-                for objet in self.__session.query(q).all():
-                    key = objet.__class__.__name__ + "." + objet.id
-                    dict[key] = objet
-    
-        else:
-            for objet in self.__session.query(cls).all():
-                key = objet.__class__.__name__ + "." + objet.id
-                dict[key] = objet
-        return dict
-    
+        """squery database session"""
+        all_dict = {}
+        for itr in classes:
+            if cls is None :
+                objs = self.__session.query(classes[itr]).all()
+                for obj in objs:
+                    key = obj.__class__.__name__ + '.' + obj.id
+                    all_dict[key] = obj
+        return (all_dict)
+
     def new(self, obj):
-        """add the object to the current database session"""
-        self.__session.add(obj)
+        """add new obj"""
+        if obj is not None:
+            self.__session.add(obj)
+            self.save()
 
     def save(self):
-        """commit all changes of the current database session"""
+        """commit changes"""
         self.__session.commit()
 
     def delete(self, obj=None):
-        """delete from the current database session obj"""
+        """delete obj if exists"""
         if obj is not None:
-            self.__session.delete(obj)
+            del obj
+            self.save()
+
     def reload(self):
-        """database (feature of SQLAlchemy"""
+        """reload"""
         Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(session_factory )
+        session = sessionmaker(
+            bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session)
         self.__session = Session()
-        
-
-
-
--- models/state.py
+++ models/state.py
@@ -1,29 +1,24 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-import models
 from models.base_model import BaseModel, Base
-from models.city import City
-from models import storage
-import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
+from models.city import City
 from sqlalchemy.orm import relationship
 from os import getenv
-    
+import models
+
+
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "States"
+    __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship('City', backref='state')
-
-if models.storage_type != "db":
-    @property
-    def cities(self):
-        """FileStorage relationship between State and City"""
-        list = []
-        for city in storage.all(City):
-            if (city.state_id == self.id):
-                 list.append(city)
-        return list
-
+        cities = relationship("City", cascade="all, delete", backref="state")
+    else:
+        @property
+        def cities(self):
+            cities = []
+            for city in models.storage.all(City).values():
+                if city.state_id == self.id:
+                    cities.append(city)
+            return cities
