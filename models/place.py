c0d02b0950fc225de94c5838dda5b1050c341051
Soniabensaad <5687@holbertonstudents.com>
2023-03-22 22:18:43 +0100

    airbnb work

--- models/place.py
+++ models/place.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models.review import Review
+import models
 from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from sqlalchemy.orm import relationship
 
 
 class Place(BaseModel, Base):
@@ -18,3 +21,14 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=False)
     longitude = Column(Float, nullable=False)
     amenity_ids = []
+    reviews = relationship("Review", backref="place", cascade="delete")
+
+    if models.storage_type != "db":
+        @property
+        def reviews(self):
+            """Get a list of all Reviews"""
+            reviewlist = []
+            for review in list(models.storage.all(Review).values()):
+                if review.place_id == self.id:
+                    reviewlist.append(review)
+            return reviewlist
