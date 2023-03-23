f9425e8da9c81f2493ccd7a0bf98af89dc2d5501
Soniabensaad <5687@holbertonstudents.com>
2023-03-23 22:08:42 +0100

    airbnb hard

--- models/amenity.py
+++ models/amenity.py
@@ -3,9 +3,15 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+from os import getenv
 
+storage = getenv("HBNB_TYPE_STORAGE")
 
 class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity")
+    if storage == 'db':
+        from models.place import place_amenity
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary="place_amenity")
+    else:
+        name = ""
