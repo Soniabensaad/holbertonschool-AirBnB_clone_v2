0307bb41d542a85afeb4a561109794d1ff1b94f2
Soniabensaad <5687@holbertonstudents.com>
2023-03-23 18:25:01 +0100

    airbnb hard

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-
+from models.engine.db_storage import DBStorage
 from os import getenv
 if getenv("HBNB_TYPE_STORAGE") == "db":
-    from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
     from models.engine.file_storage import FileStorage
