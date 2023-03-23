c7e98bda40dc8f582a447ef0b68b8b9a7230cb50
Soniabensaad <5687@holbertonstudents.com>
2023-03-23 10:48:16 +0100

    airbnb work

--- models/state.py
+++ models/state.py
@@ -12,14 +12,19 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City",  backref="state", cascade="delete")
+    cities = relationship("City", backref="state", cascade="delete")
 
-    if getenv("HBNB_TYPE_STORAGE") != "db":
+
+    if models.storage_type != "db":
         @property
         def cities(self):
-            """Get a list of all related City objects."""
-            city_list = []
-            for city in list(models.storage.all(City).values()):
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+            """getter for cities that return
+            a list of city instance equale to
+            curent state id
+            """
+            list_city = []
+            inst = models.storage.all(City)
+            for value in inst.values():
+                if value.state_id == self.id:
+                    list_city.append(value)
+            return list_city
