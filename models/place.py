9f147cf70d634b0a85454498c5926ed647a9c213
Soniabensaad <5687@holbertonstudents.com>
2023-03-23 22:03:55 +0100

    airbnb hard

--- models/place.py
+++ models/place.py
@@ -20,24 +20,22 @@ place_amenity_table = Table('place_amenity',
 
 class Place(BaseModel, Base):
     """ A place to stay """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'places'
-        city_id = Column(String(60),  ForeignKey("cities.id"), nullable=False)
-        user_id = Column(String(60),  ForeignKey("users.id"), nullable=False)
-        name = Column(String(128), nullable=False)
-        description = Column(String(1024), nullable=True)
-        number_rooms = Column(INTEGER, nullable=False, default=0)
-        number_bathrooms = Column(INTEGER, nullable=False, default=0)
-        max_guest = Column(INTEGER, nullable=False, default=0)
-        price_by_night = Column(INTEGER, nullable=False, default=0)
-        latitude = Column(Float, nullable=True)
-        longitude = Column(Float, nullable=True)
-        amenity_ids = []
-        reviews = relationship(
-            'Review', cascade="all, delete-orphan", backref='place')
-        amenities = relationship(
-            'Amenity',  secondary="place_amenity", viewonly=False)
-    
+    __tablename__ = 'places'
+    city_id = Column(String(60),  ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60),  ForeignKey("users.id"), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(INTEGER, nullable=False, default=0)
+    number_bathrooms = Column(INTEGER, nullable=False, default=0)
+    max_guest = Column(INTEGER, nullable=False, default=0)
+    price_by_night = Column(INTEGER, nullable=False, default=0)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
+    amenity_ids = []
+    reviews = relationship(
+        'Review', cascade="all, delete-orphan", backref='place')
+    amenities = relationship(
+        'Amenity',  secondary="place_amenity", viewonly=False)
     
 
     if getenv('HBNB_TYPE_STORAGE') != 'db':
