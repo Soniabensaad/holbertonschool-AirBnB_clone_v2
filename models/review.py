aef2400835cb4104c4d4539a7ec5bf057a052550
Soniabensaad <5687@holbertonstudents.com>
2023-03-23 21:41:22 +0100

    airbnb hard

--- models/review.py
+++ models/review.py
@@ -2,11 +2,22 @@
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from os import getenv
+
+
+storage = getenv("HBNB_TYPE_STORAGE")
 
 
 class Review(BaseModel, Base):
-    """ Review classto store review information """
-    __tablename__ = "reviews"
-    place_id = Column(String(1024),  nullable=False)
-    user_id = Column(String(60), ForeignKey("places.id"),  nullable=False)
-    text = Column(String(60), ForeignKey("users.id"), nullable=False)
+    '''
+        Implementation for the Review.
+    '''
+    __tablename__ = 'reviews'
+    if storage == 'db':
+        place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+        text = Column(String(1024), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
