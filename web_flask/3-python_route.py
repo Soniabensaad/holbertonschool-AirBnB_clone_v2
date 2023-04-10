3c77b45d7e252c6cc3eba8f2866892b30e9b9ccb
Soniabensaad <5687@holbertonstudents.com>
2023-04-10 16:34:18 +0200

    framework flask

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""starts a Flask web application
+    Your web application must be listening
+    on 0.0.0.0, port 5000"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """script that starts a Flask web application"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """/hbnb: display “HBNB”"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """display “C ” followed by the value of the text"""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    """display “Python ”, followed by the value of the text"""
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
