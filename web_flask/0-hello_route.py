6ea6e4aeffd4618efbcfa88f4633579849b46ab8
Soniabensaad <5687@holbertonstudents.com>
2023-04-10 13:26:19 +0200

    webframework

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """starts a Flask web application
-    Your web application must be listening 
+    Your web application must be listening
     on 0.0.0.0, port 5000"""
 from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
