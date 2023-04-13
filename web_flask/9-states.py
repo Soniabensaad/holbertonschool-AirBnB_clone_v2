ffcf9077893515a9059ba23ad63056406ba12231
Soniabensaad <5687@holbertonstudents.com>
2023-04-13 17:57:33 +0200

    hard to work

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -43,11 +43,14 @@ def states():
     states = storage.all("State")
     return render_template('9-states.html', states=states)
 
-@app.route('/states/<id>', strict_slashes=False)
-def states():
-    """fetching data from the storage engine"""
-    states = storage.all("State")
-    return render_template('9-states.html', states=states)
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id):
+    """Displays an HTML page with info about <id>, if it exists."""
+    for state in storage.all("State").values():
+        if state.id == id:
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
