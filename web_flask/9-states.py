7f31c7c9e799fa3e4295feef368d20fea740b8f9
Soniabensaad <5687@holbertonstudents.com>
2023-04-13 18:34:21 +0200

    hard to work

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 our web application must be listening on 0.0.0.0, port 5000
-You must use storage for fetching data from 
+You must use storage for fetching data from
 the storage engine (FileStorage or DBStorage) =>
  from models import storage and storage.all(...)
 To load all cities of a State:
@@ -35,6 +35,7 @@ app = Flask(__name__)
 @app.teardown_appcontext
 def teardown_db(exception):
         storage.close()
+
 
 @app.route("/states", strict_slashes=False)
 def states():
